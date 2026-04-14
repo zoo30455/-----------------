562479a81080d70719b6ac8182cd759527955dc7
Lev Lev <prisdonuts@gmail.com>
2026-04-14 16:12:40 +0300

    f_c

--- generator.py
+++ generator.py
@@ -1,21 +1,24 @@
 import random
+a=4
+b=4
+c=4
+d=4
 def generate_password(lowercase,numbers,uppercase,simbols1):
     password=[]
     abc="abcdefghijklmnopqrstuvwxyz"
     a_b_c="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if lowercase==True:
-        for i in range(4):
+        for i in range(a):
             password.append(random.choice(abc))
     if numbers==True:
-        for i in range(4):
+        for i in range(b):
             password.append(random.choice("0123456789"))
     if uppercase==True:
-        for i in range(2):
+        for i in range(c):
             password.append(random.choice(a_b_c))
     if simbols1==True:
-        for i in range(3):
+        for i in range(d):
             password.append(random.choice("!@#$%^&*()"))
     random.shuffle(password)
     password="".join(password)
-    password=(f"   {password}")
     return password
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from generator import generate_password
+import generator
 import interface
+from tkinter import messagebox
 counter=0
 def generate():
     interface.l_b.delete(0,"end")
@@ -8,6 +9,13 @@ def generate():
         state_numbers=interface.numbers.get()
         state_simbols=interface.simbols1.get()
         state_uppercase=interface.uppercase.get()
-        interface.l_b.insert("end",generate_password(state_lowercase,state_numbers,state_uppercase,state_simbols))
+        try:
+            generator.a=int(interface.en1.get())
+            generator.b=int(interface.en2.get())
+            generator.c=int(interface.en3.get())
+            generator.d=int(interface.en4.get())
+        except ValueError:
+            messagebox.showerror("Ошибка","Введите числа")
+        interface.l_b.insert("end",generator.generate_password(state_lowercase,state_numbers,state_uppercase,state_simbols))
 interface.button.config(command=generate)
 interface.window.mainloop()
